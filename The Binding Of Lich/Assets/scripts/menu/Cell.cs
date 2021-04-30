b53685676eb2f8249b3a4f0206c6437c7a2ff961
MASLOvFANTOM <48237947+MASLOvFANTOM@users.noreply.github.com>
2021-04-30 23:26:26 +0700

    Update Cell.cs
    неважная обнова

--- The Binding Of Lich/Assets/scripts/menu/Cell.cs
+++ The Binding Of Lich/Assets/scripts/menu/Cell.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Cell : MonoBehaviour
 {
     public Sprite[] grilleState = new Sprite[2];
+    public SpriteRenderer GrilleSpriteRenderer;
 
     private void Start()
     {
