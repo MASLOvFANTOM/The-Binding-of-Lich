a28eb7457792327464475d243ccd5bd4855d5362
MASLOvFANTOM <48237947+MASLOvFANTOM@users.noreply.github.com>
2021-05-04 12:17:51 +0700

    Global Update 1.0.menu
    Изменил меню, добавил JSON, Анимации, проставил почти все UI элементы

--- The Binding Of Lich/Assets/scripts/menu/Cell.cs
+++ The Binding Of Lich/Assets/scripts/menu/Cell.cs
@@ -1,12 +1,50 @@
+using System;
+using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Cell : MonoBehaviour
 {
     public Sprite[] grilleState = new Sprite[2];
-    public SpriteRenderer GrilleSpriteRenderer;
+    public Image grilleSpriteRenderer;
+    public string tag;
+    public Text statsTextDescription;
+    public Text statsTextHealth;
+    public Text statsTextDamage;
+    
+    //Json files
+    private PlayerDataOpenCell _playerDataOpenCell= new PlayerDataOpenCell();
+    private string pathToPlayerDataOpenCell;
 
     private void Start()
     {
-        
+        //задаём путь
+        pathToPlayerDataOpenCell = Path.Combine(Application.dataPath, "PlayerDataOpenCell.json");
+        if (File.Exists(pathToPlayerDataOpenCell)) // если файл найден
+        {
+            _playerDataOpenCell = JsonUtility.FromJson<PlayerDataOpenCell>(File.ReadAllText(pathToPlayerDataOpenCell));
+        }
+        else // если файл не найден
+        {
+            SavePlayerDataOpenCell();
+        }
+
+        if (tag == "knight")
+        {
+            grilleSpriteRenderer.sprite = grilleState[1];
+            // statsTextDescription.text =
+        }
     }
+
+    public void SavePlayerDataOpenCell() // сохранение изменений
+    {
+        File.WriteAllText(pathToPlayerDataOpenCell, JsonUtility.ToJson(_playerDataOpenCell));
+    }
+    
+}
+[Serializable]
+public class PlayerDataOpenCell
+{
+    public string description, heath, damage;
+    // public Array Knight =[3, 2, "dsadasd"]
 }
