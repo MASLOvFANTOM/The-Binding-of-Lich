4081490bdbdf3e0a13380ff0fefdc0bdd6f4e9f0
MASLOvFANTOM <48237947+MASLOvFANTOM@users.noreply.github.com>
2021-06-19 15:26:36 +0700

    Mini Update.Game_v1.1.Inventory
    добавил инвентарь и выпадающие предметы

--- The Binding Of Lich/Assets/scripts/Game/Inventory/InventoryItemData.cs
+++ The Binding Of Lich/Assets/scripts/Game/Inventory/InventoryItemData.cs
@@ -4,10 +4,7 @@ using UnityEngine.UI;
 [CreateAssetMenu(menuName = "items/Game Items", fileName = "game item")]
 public class InventoryItemData : ScriptableObject
 {
-    public Image mainSprite;
+    public Sprite mainSprite;
     public int meCount;
     public string description;
-    
-
-    public void Effect(){}
 }
